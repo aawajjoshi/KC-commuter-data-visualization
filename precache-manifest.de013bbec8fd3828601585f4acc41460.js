ab1cc278ba18f214c63f36bdd105e1206648f5cc
Aawaj Joshi <s521315@nwmissouri.edu>
2020-08-19 05:24:29 -0500

    Updates

---
+++ precache-manifest.de013bbec8fd3828601585f4acc41460.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ccdc157f5f8a0efed477adf86594bd",
+    "url": "/KC-commuter-data-visualization/index.html"
+  },
+  {
+    "revision": "f9c72292504755461330",
+    "url": "/KC-commuter-data-visualization/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "4844951ef952de1c58df",
+    "url": "/KC-commuter-data-visualization/static/css/main.0928de9d.chunk.css"
+  },
+  {
+    "revision": "f9c72292504755461330",
+    "url": "/KC-commuter-data-visualization/static/js/2.7d9316c8.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/KC-commuter-data-visualization/static/js/2.7d9316c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4844951ef952de1c58df",
+    "url": "/KC-commuter-data-visualization/static/js/main.15c03672.chunk.js"
+  },
+  {
+    "revision": "0d182178ba4fa26b8422",
+    "url": "/KC-commuter-data-visualization/static/js/runtime-main.0d1b66d5.js"
+  }
+]);
